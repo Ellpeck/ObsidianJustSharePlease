37a9d654e6e9c000e68fb3330d5a2b37a00b2fa4
Ellpeck <me@ellpeck.de>
2023-07-24 17:00:13 +0200

    todo

--- server/upload.php
+++ server/upload.php
@@ -1,2 +1,3 @@
 // TODO when uploading, generate and send back Guid and store in meta file
 // TODO when uploading, also generate and return a deletion password that is stored in meta file
+// TODO figure out some way to make the meta file "hidden" to the web server - maybe subfolder with denied access in apache?
