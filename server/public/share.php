cccd92487cb6bedf16c8d020e9e2ff8ab418d7e5
Ellpeck <me@ellpeck.de>
2023-08-09 20:13:44 +0200

    serve GET requests

--- server/public/share.php
+++ server/public/share.php
@@ -1,9 +1,23 @@
 <?php
-// TODO request content using GET
 
-// TODO upload using POST
-// TODO when uploading, generate and send back Guid and store in meta file alongside data
-// TODO when uploading, also generate and return a deletion password that is also stored in meta file
-
-// TODO delete using DELETE
-// TODO only accept deletions if the password matches the deletion password from the upload
+parse_str($_SERVER["QUERY_STRING"], $query);
+switch ($_SERVER["REQUEST_METHOD"]) {
+    case "GET":
+        $content = file_get_contents(dirname(getcwd()) . "/data/" . $query["file"]);
+        if (!$content) {
+            http_response_code(404);
+            break;
+        }
+        header("Content-Type: text/plain");
+        echo $content;
+        break;
+    case "POST":
+        // TODO upload using POST
+        // TODO when uploading, generate and send back Guid and store in meta file alongside data
+        // TODO when uploading, also generate and return a deletion password that is also stored in meta file
+        break;
+    case "DELETE":
+        // TODO delete using DELETE
+        // TODO only accept deletions if the password matches the deletion password from the upload
+        break;
+}
