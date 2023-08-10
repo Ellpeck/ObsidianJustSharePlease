25e2744df66d04d50d0df43af0d44839fb83d8ac
Ellpeck <me@ellpeck.de>
2023-08-10 19:47:55 +0200

    implement POST

--- server/public/share.php
+++ server/public/share.php
@@ -1,23 +1,62 @@
 <?php
 
-parse_str($_SERVER["QUERY_STRING"], $query);
 switch ($_SERVER["REQUEST_METHOD"]) {
     case "GET":
-        $content = file_get_contents(dirname(getcwd()) . "/data/" . $query["file"]);
+        parse_str($_SERVER["QUERY_STRING"], $query);
+        $content = file_get_contents(get_markdown_path($query["id"]));
         if (!$content) {
             http_response_code(404);
+            echo "Not found";
             break;
         }
         header("Content-Type: text/plain");
         echo $content;
         break;
     case "POST":
-        // TODO upload using POST
-        // TODO when uploading, generate and send back Guid and store in meta file alongside data
-        // TODO when uploading, also generate and return a deletion password that is also stored in meta file
+        $body = json_decode(file_get_contents("php://input"), true);
+        $content = $body ? $body["content"] : null;
+        if (!$content) {
+            http_response_code(400);
+            echo "No content";
+            break;
+        }
+
+        try {
+            // generate id and deletion/edit password
+            $id = random_int(0, 65535);
+            $password = random_int(32, PHP_INT_MAX);
+        } catch (Exception $e) {
+            http_response_code(500);
+            echo $e->getMessage();
+            break;
+        }
+
+        $meta = json_encode([
+            "id" => $id,
+            "deletion_password" => $password
+        ]);
+
+        // store markdown and metadata in data path
+        file_put_contents(get_markdown_path($id), $content);
+        file_put_contents(get_meta_path($id), $meta);
+
+        echo $meta;
         break;
+    case "PATCH":
+        // TODO update using PATCH
     case "DELETE":
-        // TODO delete using DELETE
-        // TODO only accept deletions if the password matches the deletion password from the upload
+        
         break;
+}
+
+function get_markdown_path(string $id): string {
+    return get_data_path() . $id . ".md";
+}
+
+function get_meta_path(string $id): string {
+    return get_data_path() . $id . ".json";
+}
+
+function get_data_path(): string {
+    return dirname(getcwd()) . "/data/";
 }
